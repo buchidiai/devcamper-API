cdbbe048d2a171a1d731e7c747406cc29d516936
54 Bridge <31743169+buchidiai@users.noreply.github.com>
2020-03-18 20:22:52 -0500

    added db config

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-//Route Files
-
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //Load
 dotenv.config({ path: "./config/config.env" });
+
+//conect to Database
+connectDB();
+
+//Route Files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -15,11 +18,18 @@ const app = express();
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 //mount routers
-
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  //close server & exit process
+  server.close(() => process.exit(1));
 });
