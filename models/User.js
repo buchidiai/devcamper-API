cdc7d5d2e181e38bdef14a07b2ea97a45e8b8287
54 Bridge <31743169+buchidiai@users.noreply.github.com>
2020-03-21 17:33:49 -0500

    updated register controller and user model with jwt

--- models/User.js
+++ models/User.js
@@ -41,4 +41,13 @@ UserSchema.pre("save", async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
 module.exports = mongoose.model("User", UserSchema);
+//statics called on the model
+//methods called on intialize from model
