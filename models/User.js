a524f1fdf0a758d78dc010a1dd10070fb188fc43
54 Bridge <31743169+buchidiai@users.noreply.github.com>
2020-03-21 17:56:41 -0500

    added login route and updated user conroller

--- models/User.js
+++ models/User.js
@@ -48,6 +48,11 @@ UserSchema.methods.getSignedJwtToken = function() {
   });
 };
 
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", UserSchema);
 //statics called on the model
 //methods called on intialize from model
